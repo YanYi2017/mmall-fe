0bcdddf7ef9bf16f3178cccc59a1d1135250d28c
YanYi <yliu_2017@163.com>
2019-03-06 17:27:06 +0800

    配置webpack.config.js，使用file-loader加载图片和文字

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,18 @@ module.exports = {
                     },
                     'css-loader'
                 ]
+            },
+            //使用file-loader加载图片和字体
+            {
+                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     },

9616cb7d5017d154f34045482136eb7a209e8bc7
YanYi <yliu_2017@163.com>
2019-03-13 18:22:46 +0800

    开发简单导航头

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,15 +1,5 @@
 'use strict';
 
 import './index.css';
+import 'Page/common/nav-simple/index.js';
 import _mm from 'Util/mm.js';
-import 'Node_modules/_@fortawesome_fontawesome-free@5.7.2@@fortawesome/fontawesome-free/css/all.min.css';
-console.log('Hello index');
-
-var data = {
-    screenName: 'ddfhg'
-};
-var template = '<div>{{screenName}}</div>';
-
-console.log(_mm.renderHTML(template, data));
-
-console.log(_mm.validate('', 'required'));

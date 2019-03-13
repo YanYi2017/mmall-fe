e550004f14f42bcb26455521cc3a34f12542889a
YanYi <yliu_2017@163.com>
2019-03-13 19:47:35 +0800

    初始化通用导航开发逻辑层

--- src/page/common/nav/index.js
+++ src/page/common/nav/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
 import './index.css';
+
+var nav = {
+    init: function () {
+        this.bindEvent();
+        this.loadCartCount();
+        this.loadUserInfo();
+        return this;
+    },
+    bindEvent: function () {},
+    loadUserInfo: function () {},
+    loadCartCount: function () {}
+};
+
+module.exports = nav();

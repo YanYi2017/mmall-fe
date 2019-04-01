69e36b715485316271045a25fe649df29e88ebec
YanYi <yliu_2017@163.com>
2019-04-01 19:56:04 +0800

    完成首页左侧关键字导航开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -3,9 +3,5 @@
 require('./index.css');
 require('Page/common/header/index.js');
 require('Page/common/nav/index.js');
-var navSide = require('Page/common/nav-side/index.js');
-var _mm = require('Util/mm.js');
 
-navSide.init({
-    name : 'about'
-});
+var _mm = require('Util/mm.js');

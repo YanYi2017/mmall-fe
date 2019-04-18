df3478d4865553dcde47aef29fad8b3c73f10a60
YanYi <yliu_2017@163.com>
2019-04-18 15:21:30 +0800

    完善商品详情页错误处理功能
    当productID不存在时，自动返回主页；
    当数据库中搜不到相应产品时，提示“产品已下架或删除”；

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -9,30 +9,36 @@ var _mm = require('Util/mm.js'),
     templateHTML = require('./index.string');
 
 var page = {
+    data : {
+        productID : _mm.getURLParam('productID') || ''
+    },
     init : function () {
         this.onload();
     },
     bindEvent : function () {},
     onload : function () {
+        //若productID不存在，则回到主页
+        if (!this.data.productID) {
+            _mm.goHome();
+        }
         this.loadDetail();
     },
     //加载商品详细数据
     loadDetail : function () {
-        var productID = _mm.getURLParam('productID'),
-            detailHTML = '',
+        var detailHTML = '',
             $pageWrap = $('.page-wrap'),
             _this = this;
         //显示loading图标
         $pageWrap.html('<div class="loading"></div>');
         // 请求商品详细数据
-        _product.getProductDetail(productID, function (res) {
+        _product.getProductDetail(_this.data.productID, function (res) {
             //修改数据类型
             _this.filter(res);
             //渲染页面
             detailHTML = _mm.renderHTML(templateHTML, res);
             $pageWrap.html(detailHTML);
         }, function (errMsg) {
-            _mm.errorTips(errMsg);
+            $pageWrap.html('<div class="err-tips">' + errMsg + '</div>');
         });
     },
     //数据处理

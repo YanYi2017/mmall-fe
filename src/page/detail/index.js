c1689701521431d3637c4636b9df5e427699ac06
YanYi <yliu_2017@163.com>
2019-04-18 10:12:21 +0800

    完成商品详情页HTML结构及样式设置

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -3,3 +3,25 @@
 require('./index.css');
 require('Page/common/nav/index.js');
 require('Page/common/header/index.js');
+
+var _mm = require('Util/mm.js'),
+    templateHTML = require('./index.string');
+
+var page = {
+    init : function () {
+        this.onload();
+    },
+    bindEvent : function () {},
+    onload : function () {
+        this.loadDetail();
+    },
+    loadDetail : function () {
+        var detailHTML = _mm.renderHTML(templateHTML, {});
+        $('.page-container').html(detailHTML);
+    }
+};
+
+$(function () {
+    page.init();
+});
+

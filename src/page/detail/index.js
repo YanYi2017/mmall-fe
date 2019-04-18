d269daa2f5cda5ca61e0076afa9aeb0bcce651f0
YanYi <yliu_2017@163.com>
2019-04-18 15:04:50 +0800

    完成商品详情页数据请求及渲染

--- src/page/detail/index.js
+++ src/page/detail/index.js
@@ -5,6 +5,7 @@ require('Page/common/nav/index.js');
 require('Page/common/header/index.js');
 
 var _mm = require('Util/mm.js'),
+    _product = require('Service/product-service.js'),
     templateHTML = require('./index.string');
 
 var page = {
@@ -15,9 +16,28 @@ var page = {
     onload : function () {
         this.loadDetail();
     },
+    //加载商品详细数据
     loadDetail : function () {
-        var detailHTML = _mm.renderHTML(templateHTML, {});
-        $('.page-container').html(detailHTML);
+        var productID = _mm.getURLParam('productID'),
+            detailHTML = '',
+            $pageWrap = $('.page-wrap'),
+            _this = this;
+        //显示loading图标
+        $pageWrap.html('<div class="loading"></div>');
+        // 请求商品详细数据
+        _product.getProductDetail(productID, function (res) {
+            //修改数据类型
+            _this.filter(res);
+            //渲染页面
+            detailHTML = _mm.renderHTML(templateHTML, res);
+            $pageWrap.html(detailHTML);
+        }, function (errMsg) {
+            _mm.errorTips(errMsg);
+        });
+    },
+    //数据处理
+    filter : function (res) {
+        res.subImages = res.subImages.split(',');
     }
 };
 
--- src/service/product-service.js
+++ src/service/product-service.js
@@ -6,10 +6,21 @@ var _product = {
     //获取产品列表
     getProductList : function (param, resolve, reject) {
         _mm.request({
-            url : _mm.getServerURL('/product/list.do'),
-            data : param,
-            success : resolve,
-            error : reject
+            url         : _mm.getServerURL('/product/list.do'),
+            data        : param,
+            success     : resolve,
+            error       : reject
+        });
+    },
+    //获取产品详细信息
+    getProductDetail : function (productID, resolve, reject) {
+        _mm.request({
+            url         : _mm.getServerURL('/product/detail.do'),
+            data        : {
+                productId : productID
+            },
+            success     : resolve,
+            error       : reject
         });
     }
 };
